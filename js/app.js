f1d75453aa3e328c247f5b28ebc5b14701f95ac9
JacobRishe <jacob.rishe@gmail.com>
2021-02-02 15:52:22 -0800

    fetch call is now working with title & is now displaying one movie

--- js/app.js
+++ js/app.js
@@ -7,31 +7,39 @@ let displayMovie = document.querySelector('p')
 //access the API and grab the data of the search and assign that data to a variable with a condition
 const getMovie = (event) => {
   let movieInput = document.getElementById('input').value
-  fetch(`http://www.omdbapi.com/?s=${movieInput}&apikey=101d95ae`)
+  fetch(`http://www.omdbapi.com/?t=${movieInput}&apikey=101d95ae`)
 //convert response to json which will return another promise
     .then(response => response.json())
   //our actual data
     .then(data => {
+      console.log(data)
    // iterate through the array data.search
-      for (let i = 0; i < data.Search.length; i++) {
-          console.log(data.Search[i])
+
+          console.log(data.Title)
           //creating a div
-          let element = document.createElement('div')
-          // assigning a class
-          element.setAttribute('class', 'card')
-          element.innerHTML = data.Search[i].Title
-          // element.innerHTML = `<img src=${data.Search[i].Poster}>`
+          let divElement = document.createElement('div')
+
+          let movieTitle = document.createElement('h3')
+          divElement.setAttribute('class', 'movieTitle')
+          divElement.textContent = data.Title
+          divElement.appendChild(movieTitle)
+
+          let movieYear = document.createElement('h3')
+          movieYear.setAttribute('class', 'movieYear')
+          movieYear.textContent = data.Year
+          divElement.appendChild(movieYear)
+
+          let movieRated = document.createElement('h3')
+          movieRated.setAttribute('class', 'Rated')
+          movieRated.textContent = data.Rated
+          divElement.appendChild(movieRated)
+
           let poster = document.createElement('img')
-          poster.setAttribute('src', data.Search[i].Poster)
-          element.appendChild(poster)
-          //or this way
-          let poster = document.createElement('img')
-          poster.setAttribute('src', data.Search[i].Poster)
-          element.appendChild(poster)
+          poster.setAttribute('src', data.Poster)
+          divElement.appendChild(poster)
           // console.log(element, i)
           //appending the div to the dom
-          document.getElementById('searchResults').appendChild(element)
-      }
+          document.getElementById('searchResults').appendChild(divElement)
 
       // append to html document
 
